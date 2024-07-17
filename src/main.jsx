4d608034b3ff6ecc6b90dd60d1de52e02b71252b
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-07-18 01:38:31 +0600

    meta tag added

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,16 @@ import "react-photo-view/dist/react-photo-view.css";
 
 import { Provider } from "react-redux";
 import store from "./app/store.js";
+import { HelmetProvider } from "react-helmet-async";
+
+const helmetContext = {};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HelmetProvider context={helmetContext}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
